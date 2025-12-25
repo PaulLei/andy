a5af8df53b65dd15b6dd35bd773a27c91b82c81c
PaulLei <92281261+PaulLei@users.noreply.github.com>
2025-12-24 21:23:57 -0500

    c

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { ArrowRight } from 'lucide-react';
 import { useEffect, useState } from 'react';
-
+//advik
 export default function Hero() {
   const [isVisible, setIsVisible] = useState(false);
 
